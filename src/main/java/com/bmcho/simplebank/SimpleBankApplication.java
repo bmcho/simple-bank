3bf86e125bd8dc36e532e691867d898c95318783
bmcho12 <bmcho12@nm-metaworld.com>
2025-06-19 15:53:30 +0900

    init

---
+++ src/main/java/com/bmcho/simplebank/SimpleBankApplication.java
@@ -0,0 +1,13 @@
+package com.bmcho.simplebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankApplication.class, args);
+    }
+
+}
